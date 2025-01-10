668ff441d7ca3ad03d515c7c01ed0569213daad6
cuzekn <cuzekn@gmail.com>
2025-01-10 15:11:31 +0900

    fix: 文字の修正

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/features/swipe_gesture.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/features/swipe_gesture.h
@@ -60,7 +60,7 @@ void process_swipe_gesture(int16_t x, int16_t y) {
       }
     }
     unregister_code(KC_LCMD);
-    unregister_code(KC_LCTRL);
+    unregister_code(KC_LCTL);
 
     register_code(KC_LALT);
   }
